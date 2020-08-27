82041d078723c136a2d6a31ad2ab457f45b76b22
Scott Million <scottmillion@gmail.com>
2020-08-27 07:15:53 +0700

    Added default todos, ability to delete and cross out todos

--- 10. jQuery ToDo List/assets/js/todos.js
+++ 10. jQuery ToDo List/assets/js/todos.js
@@ -1,1 +1,12 @@
-console.log("hellO");
+$("li").on("click", function () {
+  $(this).toggleClass("crossOff");
+});
+
+$("li span").on("click", function () {
+  console.log("working");
+  $(this)
+    .parent()
+    .fadeOut(300, "swing", function () {
+      $(this).remove();
+    });
+});
