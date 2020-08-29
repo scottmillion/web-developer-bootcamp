e6fe4efc544b9ccc3a500447c2df20b147e3c402
Scott Million <scottmillion@gmail.com>
2020-08-30 05:20:24 +0700

    finished project

--- 11. Project Patatap Clone/assets/js/circles.js
+++
@@ -1,9 +0,0 @@
-// console.log("hello");
-
-// let myCanvas = document.getElementById("myCanvas");
-
-// myCanvas.addEventListener("click", function (e) {
-//   // myCanvas.style.display = "none";
-//   console.log(e);
-//   // e.style.opacity = 1;
-// });
